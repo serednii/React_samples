8ba8f8db184cdef3271a8b78047f882ee65df0cf
Serednii Mykola <seredniimykola@gmail.com>
2022-10-23 21:28:08 +0200

    change modal window

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import './index.scss';
 
 function App() {
+  const [open, setOPen] = React.useState(false);
+  
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
+      <button onClick={()=>setOPen(true)} className="open-modal-btn">✨ Открыть окно</button>
+
+
+         <div className={`overlay  animated${open  ?  'show' : ''}` }>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+          <svg onClick={()=>setOPen(false)}  height="200" viewBox="0 0 200 200" width="200">
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
-      </div> */}
-    </div>
+      </div> 
+   
+ </div>
   );
 }
 
