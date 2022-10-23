ad298684f1d830d4abdab2c6e53e6e7cb651a59a
Serednii Mykola <seredniimykola@gmail.com>
2022-10-23 21:01:16 +0200

    change counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './index.scss';
+import React from 'react'
 
 function App() {
+
+  // let count = 0;
+  const [count, steCount] = React.useState(0);
+
+  const onClickPlus = ()=>{
+  steCount(count+1);
+  }
+  
+  const onClickMinus = ()=>{
+    steCount(count-1);
+    }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
